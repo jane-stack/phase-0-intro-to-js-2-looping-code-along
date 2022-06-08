8754ee1cf210e7ba2efc4a8fc352d876d8bdac6e
Tuyen Ha <tuyen_ha@outlook.com>
2022-06-07 21:22:22 -0600

    commit tree 769dc5c5c959be8fda61830e77e79d5a55dac009

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+    
+}
+
+console.log(writeCards(["Gaudalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
+console.log(countDown());
